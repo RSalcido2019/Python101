7439e5da0708be1b1192c28e0105a4db4590f017
RSalcido2019 <ricardo.salcido@outlook.com>
2022-02-28 20:50:21 -0600

    Conditionals in python

--- conditionals_start.py
+++ conditionals_start.py
@@ -7,7 +7,10 @@ def main():
     x, y = 10, 100
 
     # conditional flow uses if, elif, else
+    if (x < y):
+     st = "x is less than y"
 
+    print(st)
     # conditional statements let you use "a if C else b"
 
 
