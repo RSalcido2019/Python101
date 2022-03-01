390cc7722098baf6897d6fcb007877ee3fc13cb1
RSalcido2019 <ricardo.salcido@outlook.com>
2022-02-28 20:56:10 -0600

    Conditional Structures

--- conditionals_start.py
+++ conditionals_start.py
@@ -4,11 +4,13 @@
 
 
 def main():
-    x, y = 1000, 100
+    x, y = 1000, 1000
 
     # conditional flow uses if, elif, else
     if (x < y):
      st = "x is less than y"
+    elif (x == y):
+     st = "x and y are the same"
     else:
      st = "x is greater than y"
     print(st)
