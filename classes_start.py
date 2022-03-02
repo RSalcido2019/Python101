3ebd01d49ee3fb4e0d22c464cbca7480f7c7f201
RSalcido2019 <ricardo.salcido@outlook.com>
2022-03-01 20:59:11 -0600

    classes

--- classes_start.py
+++ classes_start.py
@@ -2,8 +2,17 @@
 # Example file for working with classes
 #
 
+class myClass():
+  def method1(self):
+    print("myClass method1")
+
+  def method2(self, someString):
+    print("myClass method2 " + someString)
 
 def main():
+  c = myClass()
+  c.method1()
+  c.method2("this is some Sting")
 
 
 if __name__ == "__main__":
