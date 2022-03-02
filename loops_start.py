f9fc114697c370c4ee80c964cd49b61c27ac85dd
RSalcido2019 <ricardo.salcido@outlook.com>
2022-03-01 20:46:08 -0600

    itterations

--- loops_start.py
+++ loops_start.py
@@ -15,16 +15,19 @@ def main():
     print(x)
 
   # use a for loop over a collection
-  days=["Mon","TUES","WED"]
-  for t in days:
-    print(t)
+  # days=["Mon","TUES","WED"]
+  # for t in days:
+  #   print(t)
   
   # use the break and continue statements
-  for x in range(5,10):
-    if (x == 7): break
-    print(x)
+  # for x in range(5,10):
+  #   # if (x == 7): break
+  #   if (x%2 == 0): continue
+  #   print(x)
 
   #using the enumerate() function to get index 
-  
+  days=["Mon","TUES","WED"]
+  for i, d  in enumerate(days):
+    print(i, d)
 if __name__ == "__main__":
   main()
