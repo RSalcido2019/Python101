7468373ed6dcb8fca8458db0338a2e31cd9ad259
RSalcido2019 <ricardo.salcido@outlook.com>
2022-03-02 21:09:58 -0600

    levaraging pre built libraries

--- modules_start.py
+++ modules_start.py
@@ -5,7 +5,7 @@ import math
 print("The square root of 16 is", math.sqrt(16))
 
 # in addition to functions, some modules contain useful constants
-
+print("Pi is", math.pi)
 
 # try some of the math functions for yourself here:
 
