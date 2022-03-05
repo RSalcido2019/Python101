9af4ac6da80161fc492d1811d580da153471f262
RSalcido2019 <ricardo.salcido@outlook.com>
2022-03-04 21:34:13 -0600

    datetime objects

--- dates_start.py
+++ dates_start.py
@@ -9,21 +9,21 @@ from datetime import datetime
 def main():
   ## DATE OBJECTS
   # Get today's date from the simple today() method from the date class
-  today = date.today()
-  print("Today's date is ", today)
-
-  # print out the date's individual components
-  print("Date components: ", today.day, today.month, today.year)
-  
-  # retrieve today's weekday (0=Monday, 6=Sunday)
-  print ("Today's week day number is ", today.weekday())
-  days = ["mon", "Tues", "Wed", "thur","Friday","Saturday", "Sunday"]
-  print("Which is a: ", days[today.weekday()])
+  # today = date.today()
+  # print("Today's date is ", today)
+  #
+  # # print out the date's individual components
+  # print("Date components: ", today.day, today.month, today.year)
+  #
+  # # retrieve today's weekday (0=Monday, 6=Sunday)
+  # print ("Today's week day number is ", today.weekday())
+  # days = ["mon", "Tues", "Wed", "thur","Friday","Saturday", "Sunday"]
+  # print("Which is a: ", days[today.weekday()])
 
   ## DATETIME OBJECTS
   # Get today's date from the datetime class
-
-  
+  today = datetime.now()
+  print("Today is :", today)
   # Get the current time
 
  
